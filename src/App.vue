5031695f2185c1a150c9bbd00253ea14fcb723d5
Array58 <17758033639@163.com>
2018-10-29 17:58:42 +0800

    添加文件到git

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="router_page">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  position: relative;
+  overflow-x: hidden;
+  .router_page{
+    position: absolute;
+    top: 0;
+    left: 100%;
+    width: 100%;
+    height: 100%;
+    background: yellow;
+    transition-duration:500ms; 
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <XButton>dddd</XButton>
+    <div class="test_less">
+      <div>dddddddddd</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { XButton } from 'vux'
+
+export default {
+  components: {
+    XButton
+  }
+}
+</script>
+
+<style lang="less">
+  .test_less{
+    div{
+      background: yellow;
+    }
+  }
+</style>
